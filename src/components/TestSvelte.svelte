59e254fe44565a5aad8ada9f4e7b0de6237eeec7
JD Calvelli <jdcalvelli@gmail.com>
2023-04-17 14:39:05 -0400

    basic functionality

--- src/components/TestSvelte.svelte
+++ src/components/TestSvelte.svelte
@@ -5,6 +5,10 @@
 
 <button on:click={incrementCount}> increment count </button>
 
+{#if count > 10}
+   <button on:click={incrementGenerator}> buy a generator </button>
+{/if}
+
 <!-- then the css style tag -->
 <style>
 
@@ -14,7 +18,25 @@
 <script>
   // variables are reactive by default
   let count = 0;
+  let generator = 0;
+
   function incrementCount() {
     count++;
   }
+
+  // basic functionality, no increase on cost
+  function incrementGenerator() {
+    if (count >= 10) {
+      generator++;
+      count -= 10;
+    }
+  }
+
+  // hard coded generator interval
+  setInterval(() => {
+    if (generator > 0) {
+      count += generator;
+    }
+  }, 3000);
+
 </script>
