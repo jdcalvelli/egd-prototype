f7f4870b6c6fc0d29848d57b1505c4bc74d2f846
JD Calvelli <jdcalvelli@gmail.com>
2023-04-17 15:25:03 -0400

    adding incremental cost calculation for generator

--- src/components/TestSvelte.svelte
+++ src/components/TestSvelte.svelte
@@ -1,11 +1,13 @@
 <!-- first the html -->
 <h1> testing svelte </h1>
 
-<p> current count: {count} </p>
+<p> current currency: {currency} </p>
+<p> generator count: {generator} </p>
+<p> generator cost: {generatorCost} </p>
 
-<button on:click={incrementCount}> increment count </button>
+<button on:click={incrementCurrency}> increment currency </button>
 
-{#if count > 10}
+{#if currency > 10}
    <button on:click={incrementGenerator}> buy a generator </button>
 {/if}
 
@@ -17,26 +19,35 @@
 <!-- then the js script tag -->
 <script>
   // variables are reactive by default
-  let count = 0;
+  let currency = 0;
   let generator = 0;
+  let generatorCost = 10;
 
-  function incrementCount() {
-    count++;
+  function incrementCurrency() {
+    currency++;
   }
 
   // basic functionality, no increase on cost
   function incrementGenerator() {
-    if (count >= 10) {
+    if (currency >= generatorCost) {
       generator++;
-      count -= 10;
+      currency -= generatorCost;
+      updateGeneratorCost();
     }
   }
 
+  function updateGeneratorCost() {
+    // using function generatorCost = numGenerator ^ 1.5 + 10
+    generatorCost = Math.ceil(Math.pow(generator, 1.5) + 10);
+  }
+
   // hard coded generator interval
+  // should probably change this to requestAnimationFrame
+  // bc right now it can be blocked by other activities on main thread
   setInterval(() => {
     if (generator > 0) {
-      count += generator;
+      currency += generator;
     }
-  }, 3000);
+  }, 1000);
 
 </script>
