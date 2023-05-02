bc64998b75c4afbd63429d774819121bc42b1768
JD Calvelli <jdcalvelli@gmail.com>
2023-05-02 16:31:28 -0400

    fixing display of tick decrease cost, formatting file correctly

--- src/components/IncrementalGame.svelte
+++ src/components/IncrementalGame.svelte
@@ -1,141 +1,29 @@
-<!--
-  incremental game logic
-  current capacity:
-    click to increment by increment amount
-
-    updating formula to be based on alexander formula:
-    price = baseCost * multiplier ^ numberOwned 
-
-    gen1 gives 1 currency per gen
-    gen2 gives 2 currency per gen
-    gen3 gives 4 currency per gen
-    gen4 gives 8 currency per gen
-    gen5 gives 16 currency per gen
-    gen6 gives 32 currency per gen
-
-    interval can be decremented by 50, until hitting 500
-
--->
-
-<!-- first the html -->
-
-<div class="info">
-  <h3> work completed: {currency} </h3>
-  {#if gen1Owned > 0}
-    <p> automated work per tick: {1 * gen1Owned + 2 * gen2Owned + 4 * gen3Owned + 8 * gen4Owned + 16 * gen5Owned + 32 * gen6Owned} </p>
-    <p> automated work tick speed: {updateInterval}</p>
-  {/if}
-</div>
-
-<div class="globals">
-  <button on:click={incrementCurrency}>{#if gen6Owned == 0} do work {:else} do work? why bother? you've been replaced. {/if}</button>
-  {#if currency >= intervalDecrementStartCost && updateInterval > 500}
-    <button on:click={decrementInterval}> automate work faster (cost: {intervalDecrementCost}) </button>
-  {/if}
-</div>
-
-<div class="genContainer">
-  
-  {#if currency >= gen1StartCost || gen1Owned >= 1}
-    <div class="gen">
-      <p> steam engines: {gen1Owned} </p>
-      <p> cost: {gen1Cost} </p>
-      {#if currency >= gen1Cost}
-         <button on:click={() => incrementGenerator("gen1")}> buy steam engine </button>
-      {/if}
-    </div>
-  {/if}
-  
-  {#if gen1Cost >= gen2StartCost}
-    <div class="gen">
-      <p> assembly lines: {gen2Owned} </p>
-      <p> cost: {gen2Cost} </p>
-      {#if currency >= gen2Cost}
-        <button on:click={() => incrementGenerator("gen2")}> buy assembly line </button>
-      {/if}
-    </div>
-  {/if}
-  
-  {#if gen2Cost >= gen3StartCost}
-    <div class="gen">
-      <p> computers: {gen3Owned} </p>
-      <p> cost: {gen3Cost} </p>
-      {#if currency >= gen3Cost}
-        <button on:click={() => incrementGenerator("gen3")}> buy computer </button>
-      {/if}
-    </div>
-  {/if}
-  
-  {#if gen3Cost >= gen4StartCost}
-    <div class="gen">
-      <p> machine learning models: {gen4Owned} </p>
-      <p> cost: {gen4Cost} </p>
-      {#if currency >= gen4Cost}
-        <button on:click={() => incrementGenerator("gen4")}> buy ml model </button>
-      {/if}
-    </div>
-  {/if}
-  
-  {#if gen4Cost >= gen5StartCost}
-    <div class="gen">
-      <p> large language models: {gen5Owned} </p>
-      <p> cost: {gen5Cost} </p>
-      {#if currency >= gen5Cost}
-        <button on:click={() => incrementGenerator("gen5")}> buy ll model </button>
-      {/if}
-    </div>
-  {/if}
-  
-  {#if gen5Cost >= gen6StartCost}
-    <div class="gen">
-      <p> digital overlords: {gen6Owned} </p>
-      <p> cost: {gen6Cost} </p>
-      {#if currency >= gen6Cost}
-        <button on:click={() => incrementGenerator("gen6")}> buy digital overlord </button>
-      {/if}
-    </div>
-  {/if}
-
-</div>
-
-<!-- then the css style tag -->
-<style>
-  
-  .globals {
-    padding-bottom: 64px;
-  }
-  
-  .globals {
-    display: flex;
-    gap: 16px;
-  }
-  .genContainer {
-    display: flex;
-    gap: 16px;
-    
-    height: 128px;
-    width: 100%;
-  }
-  .gen {
-    padding: 8px;
-    border: 2px solid black;
-    
-    height: 100%;
-    width: 12%;
-  }
-</style>
-
-<!-- then the js script tag -->
+<!-- the js script tag -->
 <script>
+  //   incremental game logic
+  // current capacity:
+  //  click to increment by increment amount
+  //
+  //  updating formula to be based on alexander formula:
+  //  price = baseCost * multiplier ^ numberOwned
+  //
+  //  gen1 gives 1 currency per gen
+  //  gen2 gives 2 currency per gen
+  //  gen3 gives 4 currency per gen
+  //  gen4 gives 8 currency per gen
+  //  gen5 gives 16 currency per gen
+  //  gen6 gives 32 currency per gen
+  //
+  //  interval can be decremented by 50, until hitting 500
 
   import { onMount } from "svelte";
 
   // variables are reactive by default
   let currency = 0;
   let updateInterval = 1000;
-  
+
   let incrementAmount = 1;
-  
+
   let numIntervalChanges = 0;
   let intervalDecrementAmt = 50;
   let intervalDecrementStartCost = 2048;
@@ -154,15 +42,15 @@
   let gen3Owned = 0;
   let gen3StartCost = 128;
   let gen3Cost;
-  
+
   let gen4Owned = 0;
   let gen4StartCost = 256;
   let gen4Cost;
-  
+
   let gen5Owned = 0;
   let gen5StartCost = 512;
   let gen5Cost;
-  
+
   let gen6Owned = 0;
   let gen6StartCost = 1024;
   let gen6Cost;
@@ -176,13 +64,12 @@
     gen4Cost = gen4StartCost;
     gen5Cost = gen5StartCost;
     gen6Cost = gen6StartCost;
-    
+
     intervalDecrementCost = intervalDecrementStartCost;
-    
-  })
+  });
 
   // SUDO UPDATE - COULD USE MORE WORK
-  
+
   setInterval(() => {
     if (gen1Owned > 0) {
       currency += gen1Owned;
@@ -206,14 +93,12 @@
 
   // HELPER FUNCTIONS
 
-
   function incrementCurrency() {
     currency += incrementAmount;
   }
 
   // basic functionality, no increase on cost
   function incrementGenerator(generatorName) {
-    
     switch (generatorName) {
       case "gen1":
         if (currency >= gen1Cost) {
@@ -225,9 +110,9 @@
 
       case "gen2":
         if (currency >= gen2Cost) {
-         gen2Owned++;
-         currency -= gen2Cost;
-         updateGeneratorCost("gen2");
+          gen2Owned++;
+          currency -= gen2Cost;
+          updateGeneratorCost("gen2");
         }
         break;
 
@@ -238,14 +123,14 @@
           updateGeneratorCost("gen3");
         }
         break;
-        
+
       case "gen4":
         if (currency >= gen4Cost) {
           gen4Owned++;
           currency -= gen4Cost;
           updateGeneratorCost("gen4");
         }
-        
+
       case "gen5":
         if (currency >= gen5Cost) {
           gen5Owned++;
@@ -253,19 +138,18 @@
           updateGeneratorCost("gen5");
         }
         break;
-      
+
       case "gen6":
-      if (currency >= gen6Cost) {
-        gen6Owned++;
-        currency -= gen6Cost;
-        updateGeneratorCost("gen6");
-      }
-      break;
-    
+        if (currency >= gen6Cost) {
+          gen6Owned++;
+          currency -= gen6Cost;
+          updateGeneratorCost("gen6");
+        }
+        break;
+
       default:
         break;
     }
-
   }
 
   function decrementInterval() {
@@ -281,29 +165,41 @@
     // using base function in above comment
     switch (generatorName) {
       case "gen1":
-        gen1Cost = Math.ceil(gen1StartCost * Math.pow(valueMultiplier, gen1Owned))
+        gen1Cost = Math.ceil(
+          gen1StartCost * Math.pow(valueMultiplier, gen1Owned)
+        );
         break;
 
       case "gen2":
-        gen2Cost = Math.ceil(gen2StartCost * Math.pow(valueMultiplier, gen2Owned));
+        gen2Cost = Math.ceil(
+          gen2StartCost * Math.pow(valueMultiplier, gen2Owned)
+        );
         break;
 
       case "gen3":
-        gen3Cost = Math.ceil(gen3StartCost * Math.pow(valueMultiplier, gen3Owned));
+        gen3Cost = Math.ceil(
+          gen3StartCost * Math.pow(valueMultiplier, gen3Owned)
+        );
         break;
-        
+
       case "gen4":
-        gen4Cost = Math.ceil(gen4StartCost * Math.pow(valueMultiplier, gen4Owned));
+        gen4Cost = Math.ceil(
+          gen4StartCost * Math.pow(valueMultiplier, gen4Owned)
+        );
         break;
-        
+
       case "gen5":
-        gen5Cost = Math.ceil(gen5StartCost * Math.pow(valueMultiplier, gen5Owned));
+        gen5Cost = Math.ceil(
+          gen5StartCost * Math.pow(valueMultiplier, gen5Owned)
+        );
         break;
-        
+
       case "gen6":
-        gen6Cost = Math.ceil(gen6StartCost * Math.pow(valueMultiplier, gen6Owned));
+        gen6Cost = Math.ceil(
+          gen6StartCost * Math.pow(valueMultiplier, gen6Owned)
+        );
         break;
-    
+
       default:
         break;
     }
@@ -311,7 +207,140 @@
 
   function updateIntervalDecrementCost() {
     // using base function interval cost = last interval cost + 2 * numIntervalChanges ^ 1.5
-    intervalDecrementStartCost = Math.ceil(intervalDecrementStartCost * Math.pow(valueMultiplier, numIntervalChanges))
+    intervalDecrementCost = Math.ceil(
+      intervalDecrementStartCost * Math.pow(valueMultiplier, numIntervalChanges)
+    );
+  }
+</script>
+
+<!-- the html -->
+
+<div class="info">
+  <h3>work completed: {currency}</h3>
+  {#if gen1Owned > 0}
+    <p>
+      automated work per tick: {1 * gen1Owned +
+        2 * gen2Owned +
+        4 * gen3Owned +
+        8 * gen4Owned +
+        16 * gen5Owned +
+        32 * gen6Owned}
+    </p>
+    <p>automated work tick speed: {updateInterval}</p>
+  {/if}
+</div>
+
+<div class="globals">
+  <button on:click={incrementCurrency}
+    >{#if gen6Owned == 0}
+      do work
+    {:else}
+      do work? why bother? you've been replaced.
+    {/if}</button
+  >
+  {#if currency >= intervalDecrementStartCost && updateInterval > 500}
+    <button on:click={decrementInterval}>
+      automate work faster (cost: {intervalDecrementCost})
+    </button>
+  {/if}
+</div>
+
+<div class="genContainer">
+  {#if currency >= gen1StartCost || gen1Owned >= 1}
+    <div class="gen">
+      <p>steam engines: {gen1Owned}</p>
+      <p>cost: {gen1Cost}</p>
+      {#if currency >= gen1Cost}
+        <button on:click={() => incrementGenerator("gen1")}>
+          buy steam engine
+        </button>
+      {/if}
+    </div>
+  {/if}
+
+  {#if gen1Cost >= gen2StartCost}
+    <div class="gen">
+      <p>assembly lines: {gen2Owned}</p>
+      <p>cost: {gen2Cost}</p>
+      {#if currency >= gen2Cost}
+        <button on:click={() => incrementGenerator("gen2")}>
+          buy assembly line
+        </button>
+      {/if}
+    </div>
+  {/if}
+
+  {#if gen2Cost >= gen3StartCost}
+    <div class="gen">
+      <p>computers: {gen3Owned}</p>
+      <p>cost: {gen3Cost}</p>
+      {#if currency >= gen3Cost}
+        <button on:click={() => incrementGenerator("gen3")}>
+          buy computer
+        </button>
+      {/if}
+    </div>
+  {/if}
+
+  {#if gen3Cost >= gen4StartCost}
+    <div class="gen">
+      <p>machine learning models: {gen4Owned}</p>
+      <p>cost: {gen4Cost}</p>
+      {#if currency >= gen4Cost}
+        <button on:click={() => incrementGenerator("gen4")}>
+          buy ml model
+        </button>
+      {/if}
+    </div>
+  {/if}
+
+  {#if gen4Cost >= gen5StartCost}
+    <div class="gen">
+      <p>large language models: {gen5Owned}</p>
+      <p>cost: {gen5Cost}</p>
+      {#if currency >= gen5Cost}
+        <button on:click={() => incrementGenerator("gen5")}>
+          buy ll model
+        </button>
+      {/if}
+    </div>
+  {/if}
+
+  {#if gen5Cost >= gen6StartCost}
+    <div class="gen">
+      <p>digital overlords: {gen6Owned}</p>
+      <p>cost: {gen6Cost}</p>
+      {#if currency >= gen6Cost}
+        <button on:click={() => incrementGenerator("gen6")}>
+          buy digital overlord
+        </button>
+      {/if}
+    </div>
+  {/if}
+</div>
+
+<!-- css style tag -->
+<style>
+  .globals {
+    padding-bottom: 64px;
   }
 
-</script>
+  .globals {
+    display: flex;
+    gap: 16px;
+  }
+  .genContainer {
+    display: flex;
+    gap: 16px;
+
+    height: 128px;
+    width: 100%;
+  }
+  .gen {
+    padding: 8px;
+    border: 2px solid black;
+
+    height: 100%;
+    width: 12%;
+  }
+</style>
